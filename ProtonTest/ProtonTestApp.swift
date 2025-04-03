29b9210b8f4f36d17951255ff00f11a86db874d8
Andy Topley <andy@andytopley.co.uk>
2025-04-03 13:26:01 +0100

    Initial Commit

---
+++ ProtonTest/ProtonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtonTestApp.swift
+//  ProtonTest
+//
+//  Created by Andy Topley on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProtonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
